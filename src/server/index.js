da5781f8e674b5171df00afb980aaa1a75d9674c
cmargleon <cmargleon@gmail.com>
2018-06-19 16:49:04 -0400

    s

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
 import morgan from "morgan";
 
 const app = express();
+const router = express.Router();
 const DEBUG = process.env.NODE_ENV !== 'production';
 const PORT = DEBUG ? '3000' : process.env.PORT;
 
@@ -10,6 +12,18 @@ app.use(express.static(__dirname + '/../../public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/test', function(req, res) {
+    res.sendStatus(200);
+});
+
+app.get('/test2', function(req, res) {
+    res.send("200");
+});
+
+app.get('/test3', function(req, res) {
+    res.sendStatus(200).send("funciona");
+});
 
 const server = app.listen(PORT, function () {
     console.log('Express listening on port %s', PORT);
