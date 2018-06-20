067373667f2b580185d2f8e52026a1e500702970
claudio <cmargleon@gmail.com>
2018-06-20 13:58:19 -0400

    da

--- src/server/network.js
+++ src/server/network.js
@@ -17,45 +17,6 @@ let businessNetworkConnection;
 let businessNetworkName = 'degree';
 let factory;
 
-export async function registerGraduate(cardId, graduateRut,firstName, lastName, email, phoneNumber) {
-    try {
-  
-        //connect as admin
-        businessNetworkConnection = new BusinessNetworkConnection();
-        await businessNetworkConnection.connect('admin@degree');
-  
-        //get the factory for the business network
-        factory = businessNetworkConnection.getBusinessNetwork().getFactory();
-  
-        //create graduate participant
-        const graduate = factory.newResource(namespace, 'Graduate', graduateRut);
-        graduate.firstName = firstName;
-        graduate.lastName = lastName;
-        graduate.email = email;
-        graduate.phoneNumber = phoneNumber;
-        
-  
-        //add graduate participant
-        const participantRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.Graduate');
-        await participantRegistry.add(graduate);
-  
-        //issue identity
-        const identity = await businessNetworkConnection.issueIdentity(namespace + '.Graduate#' + graduateRut, cardId);
-  
-        //import card for identity
-        await importCardForIdentity(cardId, identity);
-  
-        //disconnect
-        await businessNetworkConnection.disconnect('admin@degree');
-  
-        return true;
-      }
-      catch(err) {
-        //print and return error
-        console.log(err);
-        var error = {};
-        error.error = err.message;
-        return error;
-      }
-  
-    }
+export function registerGraduate(cardId, graduateRut,firstName, lastName, email, phoneNumber) {
+  console.log('hello world')
+}
