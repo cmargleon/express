7556caba37f47f4552990498565e5bfa39b9362a
cmargleon <cmargleon@gmail.com>
2018-06-19 21:35:07 -0400

    dsa

--- src/server/index.js
+++ src/server/index.js
@@ -66,58 +66,6 @@ async function importCardForIdentity(cardName, identity) {
     await adminConnection.importCard(cardName, card);
   }
 
-  /*
-    * Create Graduate participant and import card for identity
-    * @param {String} cardId Import card id for Graduate
-    * @param {String} graduateRut Graduate account number as identifier on network
-    * @param {String} firstName Graduate first name
-    * @param {String} lastName Graduate last name
-    * @param {String} phoneNumber Graduate phone number
-    * @param {String} email Graduate email
-    */
-   async function registerGraduate(cardId, graduateRut,firstName, lastName, email, phoneNumber) {
-    try {
-
-      //connect as admin
-      businessNetworkConnection = new BusinessNetworkConnection();
-      await businessNetworkConnection.connect('admin@degree');
-
-      //get the factory for the business network
-      factory = businessNetworkConnection.getBusinessNetwork().getFactory();
-
-      //create graduate participant
-      const graduate = factory.newResource(namespace, 'Graduate', graduateRut);
-      graduate.firstName = firstName;
-      graduate.lastName = lastName;
-      graduate.email = email;
-      graduate.phoneNumber = phoneNumber;
-      
-
-      //add graduate participant
-      const participantRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.Graduate');
-      await participantRegistry.add(graduate);
-
-      //issue identity
-      const identity = await businessNetworkConnection.issueIdentity(namespace + '.Graduate#' + graduateRut, cardId);
-
-      //import card for identity
-      await importCardForIdentity(cardId, identity);
-
-      //disconnect
-      await businessNetworkConnection.disconnect('admin@degree');
-
-      return true;
-    }
-    catch(err) {
-      //print and return error
-      console.log(err);
-      var error = {};
-      error.error = err.message;
-      return error;
-    }
-
-  }
-
   
 
 app.get('/test', function(req, res) {
