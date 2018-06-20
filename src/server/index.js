e845fa7a71fb04e96f20d506aa877f32903a53a4
claudio <cmargleon@gmail.com>
2018-06-20 13:15:45 -0400

    da

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
 import morgan from "morgan";
+import { helloWorld } from './network'
 //import { registerGraduate } from './network/network.js' // ES modules
 //var network = require('./network/network.js');
 /*
