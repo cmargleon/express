a82cdbaeeeb08d2ee9234dc9694a10d0f8dc5130
claudio <cmargleon@gmail.com>
2018-06-20 12:15:58 -0400

    da

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
 import morgan from "morgan";
+import composerAdmin from "composer-admin";
 
 const app = express();
 const router = express.Router();
